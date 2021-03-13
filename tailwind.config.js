0c364b1faf4949bfb4cc7313a4a4f4a87eb35e46
muhrusdi <mhmrofficial@gmail.com>
2021-03-13 09:42:11 +0800

    add soon page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,18 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     container: {
       center: true,
       padding: "1.5rem"
+    },
+    extend: {
+      colors: {
+        secondary: {
+          900: "#44d2ff"
+        }
+      }
     }
   },
   variants: {},
